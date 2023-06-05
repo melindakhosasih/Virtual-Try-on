2579ce00cc9eac5210bcfe35af0ef004890b1f5e
109000162 <0calvwe681@gmail.com>
2023-06-05 14:21:12 +0800

    update AR

---
+++ Assets/Scripts/ARManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ARManager : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    public GameObject btnObj;
+    public GameObject btnTextObj;
+
+    private Button btn;
+    private Text btnText;
+    private Vector3[] localScale_ = {
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero,
+        Vector3.zero
+    };
+
+    [SerializeField] SliderManager sliderManager_;
+
+    private int currentModelNum = 1;
+
+    ARTrackedImageManager aRTrackedImageManager;
+    Dictionary<string, GameObject> images2prefabs = new Dictionary<string, GameObject>();
+
+    void Start(){
+        
+    }
+
+    private void Awake() {
+        foreach(GameObject prefab in prefabs) {
+            GameObject _prefab = Instantiate(prefab, prefab.transform.position, prefab.transform.rotation);
+            _prefab.name = prefab.name;
+            _prefab.SetActive(false);
+            images2prefabs.Add(_prefab.name, _prefab);
+            localScale_[currentModelNum-1] = prefab.transform.localScale;
+            currentModelNum += 1;
+        }
+        currentModelNum = 1;
+
+        btn = btnObj.GetComponent<Button>();
+        btnText = btnTextObj.GetComponent<Text>();
+        btn.onClick.AddListener(()=>{
+            images2prefabs["watch"+currentModelNum.ToString()].SetActive(false);
+            currentModelNum += 1;
+            if(currentModelNum>2){
+                currentModelNum = 1;
+            }
+            sliderManager_.initModel(images2prefabs["watch"+currentModelNum.ToString()]);
+            btnText.text = currentModelNum.ToString();
+        });
+
+        aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+        sliderManager_.initModel(images2prefabs["watch"+currentModelNum.ToString()]);
+
+    }
+
+    private void OnEnable() {
+        aRTrackedImageManager.trackedImagesChanged += ImagesChanged;
+    }
+
+    private void OnDisable() {
+        aRTrackedImageManager.trackedImagesChanged -= ImagesChanged;
+    }
+
+    private void ImagesChanged(ARTrackedImagesChangedEventArgs args) {
+        foreach(ARTrackedImage trackedImage in args.added){
+            TrackedImageUpdate(trackedImage);
+        }
+        foreach(ARTrackedImage trackedImage in args.updated){
+            TrackedImageUpdate(trackedImage);
+        }
+        foreach(ARTrackedImage trackedImage in args.removed){
+            TrackedImageRemove(trackedImage);
+        }
+    }
+
+    private void TrackedImageUpdate(ARTrackedImage image) {
+        GameObject _prefab = images2prefabs[image.referenceImage.name+currentModelNum.ToString()];
+        _prefab.transform.position = image.transform.position;
+        _prefab.transform.localScale = _prefab.transform.localScale;
+        // _prefab.transform.position = image.transform.position + new Vector3(0, _prefab.transform.position.y, _prefab.transform.position.z);
+        // _prefab.transform.rotation = Quaternion.identity*_prefab.transform.rotation;
+        _prefab.transform.rotation = _prefab.transform.rotation;
+        _prefab.SetActive(true);
+        // btnText.text = btnText.text + _prefab.transform.position;
+    }
+
+    private void TrackedImageRemove(ARTrackedImage image) {
+        images2prefabs[image.name+currentModelNum].SetActive(false);
+    }
+}
--- Assets/Scripts/SliderManager.cs
+++ Assets/Scripts/SliderManager.cs
@@ -10,26 +10,62 @@ public class SliderManager : MonoBehaviour
     [SerializeField] private Text modelScale;
 
     private Vector3 initialModelScale;
+    private Quaternion initialModelRotation;
 
     void Start(){
         initialModelScale = model_.transform.localScale;
+        initialModelRotation = model_.transform.rotation;
 
-        slider_.onValueChanged.AddListener((v) => {
-            model_.transform.localScale = initialModelScale + new Vector3(v, v, v);
-            // modelScale.text = model_.transform.localScale.ToString("0.00") + 
-            //                     model_.transform.rotation.ToString("0.00");
-            if(model_.transform.localScale.x < 0){
-                model_.transform.localScale = Vector3.zero;
-            }
-        });
+        if(this.gameObject.name == "scale"){
+            slider_.onValueChanged.AddListener((v) => {
+                GameObject modelObj = this.model_;
+                modelObj.transform.localScale = initialModelScale + new Vector3(v, v, v);
+                modelScale.text = modelObj.transform.localScale.ToString("0.00") + 
+                                    modelObj.transform.rotation.ToString("000.00");
+                if(modelObj.transform.localScale.x < 0){
+                    modelObj.transform.localScale = Vector3.zero;
+                }
+            });
+        }
+
+        if(this.gameObject.name == "rotate"){
+            slider_.onValueChanged.AddListener((v) => {
+                GameObject modelObj = this.model_;
+                modelScale.text = modelObj.transform.localScale.ToString("0.00") + 
+                                    modelObj.transform.rotation.ToString("000.00");
+                modelObj.transform.rotation = initialModelRotation*Quaternion.Euler(v, 0, 0);
+            });
+        }
     }
 
     void Update()
     {
-        
+        modelScale.text = model_.transform.localScale.ToString("0.00") + 
+                                    model_.transform.rotation.ToString("000.00") + model_.name;
     }
 
     public void initModel(GameObject model){
         this.model_ = model;
+        initialModelScale = model_.transform.localScale;
+        initialModelRotation = model_.transform.rotation;
+
+        // if(this.gameObject.name == "scale"){
+        //     slider_.onValueChanged.AddListener((v) => {
+        //         model_.transform.localScale = initialModelScale + new Vector3(v, v, v);
+        //         modelScale.text = model_.transform.localScale.ToString("0.00") + 
+        //                             model_.transform.rotation.ToString("000.00");
+        //         if(model_.transform.localScale.x < 0){
+        //             model_.transform.localScale = Vector3.zero;
+        //         }
+        //     });
+        // }
+
+        // if(this.gameObject.name == "rotate"){
+        //     slider_.onValueChanged.AddListener((v) => {
+        //         modelScale.text = model_.transform.localScale.ToString("0.00") + 
+        //                             model_.transform.rotation.ToString("000.00");
+        //         model_.transform.rotation = initialModelRotation*Quaternion.Euler(v, 0, 0);
+        //     });
+        // }
     }
 }
